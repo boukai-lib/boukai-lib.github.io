c7c8cd63391521b0854569420c3a07e6a78d3046
boukai-lib <kouhou.boukai-lib@outlook.com>
2026-01-17 10:19:23 +0900

    koushin map information

--- game/apps/libmaps/script.ts
+++ game/apps/libmaps/script.ts
@@ -13,9 +13,13 @@ const LOCATIONS: MapLocation[] = [
 	{ id: 'l3', name: '大型本/SDGs', description: '大型の絵本や、SDGsに関する資料があります。<br>ただし、借りれるかは別である。', left: 33, top: 8.5 },
 	{ id: 'l4', name: '日本文学(単行本)', description: '日本文学のうち、単行本サイズのものがあります。<br>青い鳥文庫みたいな児童向け文庫もここにあったりします。"ワンピース"とか。', left: 65, top: 8.5 },
     { id: 'l5', name: '外<br>国<br>文<br>学<br><br>単<br>行<br>本', name_s:'外国文学(単行本)', description: '"ハリーポッター"とかの外国の文学がここにあります。ディズニーは別。', left: 96.75, top: 50 },
-    { id: 'l6', name: '明石', description: '明石に関する本が置いてあります。<br><small>ここにある本を読む人はいるのでしょうか...?</small>', left: 96.75, top: 80 },
+    { id: 'l6', name: '明石ブックリスト', description: '<a href=http://www.edi.akashi.hyogo.jp/kyoiku/iinkai/masterplan2/pdf/plan_data.pdf>明石ブックリスト</a>の本が置いてあります。<br><small>ここにある本を読む人はいるのでしょうか...?</small>', left: 96.75, top: 80 },
 	{ id: 'l7', name: '日本文学(文庫)', description: '日本文学のうち、文庫サイズのものが置いてあります。<br>単行本よりも、薄く、軽く、持ちやすいです。' , left: 55, top: 94},
 	{ id: 'l8', name: '外国文学(文庫)', description: '外国文学のうち、文庫サイズのものが置いてあります。<br>ディズニーはここ。', left: 33, top: 94 },
+	{ id: 'l9', name: '事典', description: '事典が置いてあります。日本語がわからない日本語話者の方はどうぞ。', left: 65, top: 22 },
+	{ id: 'l10', name: 'マンガ', description: 'マンガが置いてあります。漫画って言っても手塚治虫とかですね。', left: 80, top: 94 },
+	{ id: 'l11', name: '新<br>刊<br>・<br>人<br>気<br>作<br>・<br>映<br>像<br>化<br>作<br>品', name_s: '新刊・人気作・映像化作品', description: '新規入荷作品、人気作品、映像化（アニメ・実写）された作品たちです。<br>"ｎ分後シリーズ"や"ソードアート・オンライン"、"薬屋のひとりごと"などはここです。', left: 2, top: 47 },
+	
 ]
 
 const map = document.getElementById('map') as HTMLElement
